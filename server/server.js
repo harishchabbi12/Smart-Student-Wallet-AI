dba3de9292611dd5e198bbe8cc10eed27242274f
harishchabbi <chabbiharish83@gmail.com>
2026-07-27 22:38:59 +0530

    chore: production-ready backend

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const protectedRoutes = require("./routes/protectedRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
@@ -10,13 +11,13 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 const aiRoutes = require("./routes/aiRoutes");
 
 dotenv.config();
-console.log(process.env.GEMINI_API_KEY);
-console.log(process.env.MONGODB_URI);
+
 connectDB();
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -26,11 +27,11 @@ app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/ai", aiRoutes);
 
 app.get("/", (req, res) => {
-    res.send("🚀 Smart Student Wallet AI Backend is Running...");
+  res.send("🚀 Smart Student Wallet AI Backend is Running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
