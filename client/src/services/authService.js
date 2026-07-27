a6b9db7b786f6fea46c02d42e24384788bd9c916
harishchabbi <chabbiharish83@gmail.com>
2026-07-27 22:35:32 +0530

    chore: configure production API URL

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/auth`
+  baseURL: `${import.meta.env.VITE_API_URL}/auth`,
 });
 
 export const loginUser = (userData) => {
-  return API.post("/auth/login", userData);
+  return API.post("/login", userData);
 };
 
 export const registerUser = (userData) => {
-  return API.post("/auth/register", userData);
+  return API.post("/register", userData);
 };
